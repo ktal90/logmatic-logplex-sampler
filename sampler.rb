445f03389735d591a6ab6b4f4fb421d05acf3abc
Guillaume Polaert <guillaume@logmatic.io>
2016-10-19 13:22:02 +0200

    tuneling ok

--- sampler.rb
+++ sampler.rb
@@ -1,62 +1,33 @@
 require 'sinatra'
 require 'net/http'
 require 'uri'
-
-require 'logger'
 require 'json'
 
+gl = 10
 
-enable :logging, :dump_errors, :raise_errors
+post '/sampler/:rate/:token' do
 
-
-=begin
-  t = %w[text/css text/html application/javascript]
-  request.accept              # ['text/html', '*/*']
-  request.accept? 'text/xml'  # true
-  request.preferred_type(t)   # 'text/html'
-  request.body                # request body sent by the client (see below)
-  request.scheme              # "http"
-  request.script_name         # "/example"
-  request.path_info           # "/foo"
-  request.port                # 80
-  request.request_method      # "GET"
-  request.query_string        # ""
-  request.content_length      # length of request.body
-  request.media_type          # media type of request.body
-  request.host                # "example.com"
-  request.get?                # true (similar methods for other verbs)
-  request.form_data?          # false
-  request["some_param"]       # value of some_param parameter. [] is a shortcut to the params hash.
-  request.referrer            # the referrer of the client or '/'
-  request.user_agent          # user agent (used by :agent condition)
-  request.cookies             # hash of browser cookies
-  request.xhr?                # is this an ajax request?
-  request.url                 # "http://example.com/example/foo"
-  request.path                # "/example/foo"
-  request.ip                  # client IP address
-  request.secure?             # false (would be true over ssl)
-  request.forwarded?          # true (if running behind a reverse proxy)
-  request.env                 # raw env hash handed in by Rack
-=end
-
-post '/' do
-
-  log = Logger.new(STDOUT)
-  log.info "here"
-  uri = URI.parse("https://api.logmatic.io/v1/input/zYZIRBwJTZWGKlLwdpj3fQ?&appname=sense360")
+  # config
+  uri = URI.parse("https://api.logmatic.io/v1/input")
 
 
   # Create the HTTP objects
   proxy_http = Net::HTTP.new(uri.host, uri.port)
   proxy_http.use_ssl = true
+  proxy_request = Net::HTTP::Post.new("#{uri.path}/#{params['token']}?#{request.query_string}", {'Content-Type' => 'application/json'})
+  # proxy_request.body = request.read_body
 
-
-  proxy_request = Net::HTTP::Post.new(uri.request_uri, {'Content-Type': 'text/plain'})
+  lines = []
+  gl = gl + 1
+  request.body.each do |line|
+    lines.push line
+  end
+  proxy_request.body = lines.to_json
 
   #proxy_request.body = ",lknkkn" #request.body
 
-  # Send the request
+  # Send the request to Logmatic.io
   proxy_response = proxy_http.request(proxy_request)
 
-  "ended #{uri.request_uri} // #{proxy_response}"
+  "#{gl}:  #{lines.to_json} // #{proxy_response}"
 end
