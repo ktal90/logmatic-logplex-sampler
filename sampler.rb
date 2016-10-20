b495090a6036ac387a55aa095a119954734cbc49
Renaud Boutet <renaudboutet@renauds-macbook-pro.home>
2016-10-20 13:42:37 +0200

    Added logging

--- sampler.rb
+++ sampler.rb
@@ -6,6 +6,8 @@ scale_factor = 1000
 
 post '/sampler/:rate/:token' do
 
+  time = Time.now.getutc
+  puts time + " received call"
 
   sample_rate = 100 / params["rate"].to_i
   sample_rate = 1 if sample_rate < 0 || sample_rate > 100
@@ -28,6 +30,8 @@ post '/sampler/:rate/:token' do
 
       proxy_request.body = request.body.read
       proxy_http.request proxy_request
-
+      puts time + " pushed"
+  else
+      puts time + " dropped"
   end
 end
