ec78eee414dd27ad3bf138573eb862245d79cf64
Guillaume Polaert <guillaume@logmatic.io>
2016-10-20 14:48:59 +0200

    unicorn

--- sampler.rb
+++ sampler.rb
@@ -4,6 +4,11 @@ require 'thread'
 call_number = 0
 scale_factor = 1000
 
+get "/loaderio-61161d1f086423cc02a6cbb28bd9a2e5" do
+  File.read('loaderio-61161d1f086423cc02a6cbb28bd9a2e5.txt')
+end
+
+
 post '/sampler/:rate/:token' do
 
   time = Time.now.getutc
@@ -19,20 +24,20 @@ post '/sampler/:rate/:token' do
   call_number = 0 if call_number == scale_factor
   if (scale_factor * call_number % sample_rate == 0)
 
-      # config
-      endpoint = "https://api.logmatic.io/v1/input/#{params['token']}/?#{request.query_string}"
-      uri = URI.parse(endpoint)
+    # config
+    endpoint = "https://api.logmatic.io/v1/input/#{params['token']}/?#{request.query_string}"
+    uri = URI.parse(endpoint)
 
-      # Create the HTTP objects
-      proxy_http = Net::HTTP.new(uri.host, uri.port)
-      proxy_http.use_ssl = true
-      proxy_request = Net::HTTP::Post.new(endpoint, {"Content-Type" => "application/logplex-1"})
+    # Create the HTTP objects
+    proxy_http = Net::HTTP.new(uri.host, uri.port)
+    proxy_http.use_ssl = true
+    proxy_request = Net::HTTP::Post.new(endpoint, {"Content-Type" => "application/logplex-1"})
 
 
-      proxy_request.body = request.body.read
-      proxy_http.request proxy_request
-      puts time.to_s + " pushed"
+    proxy_request.body = request.body.read
+    proxy_http.request proxy_request
+    puts time.to_s + " pushed"
   else
-      puts time.to_s + " dropped"
+    puts time.to_s + " dropped"
   end
 end
