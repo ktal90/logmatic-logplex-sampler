3a33edbb037e4a1265d76b07391a9885decb178e
Guillaume Polaert <guillaume@logmatic.io>
2016-10-20 09:33:16 +0200

    add threads

--- sampler.rb
+++ sampler.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'thread'
 
 call_number = 0
 scale_factor = 1000
@@ -16,17 +17,21 @@ post '/sampler/:rate/:token' do
   call_number = 0 if call_number == scale_factor
   if (scale_factor * call_number % sample_rate == 0)
 
-    # config
-    endpoint = "https://api.logmatic.io/v1/input/#{params['token']}/?#{request.query_string}"
-    uri = URI.parse(endpoint)
+    Thread.new {
 
-    # Create the HTTP objects
-    proxy_http = Net::HTTP.new(uri.host, uri.port)
-    proxy_http.use_ssl = true
-    proxy_request = Net::HTTP::Post.new(endpoint, {"Content-Type" => "application/logplex-1"})
+      # config
+      endpoint = "https://api.logmatic.io/v1/input/#{params['token']}/?#{request.query_string}"
+      uri = URI.parse(endpoint)
 
-    proxy_request.body = request.body.read
-    res = proxy_http.request proxy_request
-    return res.read_body
+      # Create the HTTP objects
+      proxy_http = Net::HTTP.new(uri.host, uri.port)
+      proxy_http.use_ssl = true
+      proxy_request = Net::HTTP::Post.new(endpoint, {"Content-Type" => "application/logplex-1"})
+
+      proxy_request.body = request.body.read
+      proxy_http.request proxy_request
+
+    }
+
   end
 end
